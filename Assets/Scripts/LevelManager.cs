e06e1aa352faab072cf65c67aa52808343a4f036
Cullen Mooney <cullen@protonmail.com>
2018-03-27 18:17:20 -0500

    level manager and music good to go

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load request for " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitGame() {
+		Debug.Log("Quitting Game");
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if (instance != null) {
+			// Removing duplicates of music player upon reload of main menu
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
